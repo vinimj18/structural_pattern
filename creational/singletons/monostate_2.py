91110c12ebd8160bb3a24fd5ef121b55684ee8e8
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-03 14:39:53 +0200

    Create monostate_2.py

---
+++ creational/singletons/monostate_2.py
@@ -0,0 +1,35 @@
+class StringReprMixin:
+    def __str__(self) -> str:
+        params = ', '.join([f'{k}={v}' for k, v in self.__dict__.items()])
+        return f'{self.__class__.__name__}({params})'
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class MonoState(StringReprMixin):
+    _state: dict = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        obj.__dict__ = cls._state
+        return obj
+
+    def __init__(self, nome=None, sobrenome=None) -> None:
+
+        if nome is not None:
+            self.nome = nome
+
+        if sobrenome is not None:
+            self.sobrenome = sobrenome
+
+
+class A(MonoState):
+    pass
+
+
+if __name__ == '__main__':
+    m1 = MonoState(nome='Vinicius')
+    m2 = A(sobrenome='Silva')
+    print(m1)
+    print(m2)
