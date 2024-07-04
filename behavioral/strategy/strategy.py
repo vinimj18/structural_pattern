1b41153fe15b5bbb6094b7f94f7df2788318a6c2
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-04 13:47:06 +0200

    Update strategy.py

--- behavioral/strategy/strategy.py
+++ behavioral/strategy/strategy.py
@@ -1,3 +1,13 @@
+"""
+Strategy é um padrão de projeto comportamental que tem
+a intenção de definir uma família de algoritmos,
+encapsular cada uma delas e torná-las intercambiáveis.
+Strategy permite que o algorítmo varie independentemente
+dos clientes que o utilizam.
+
+Princípio do aberto/fechado (Open/closed principle)
+Entidades devem ser abertas para extensão, mas fechadas para modificação
+"""
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
